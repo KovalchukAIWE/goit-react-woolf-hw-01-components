ba3e355815cafe15360032cce8575852b70b8ddf
Andrii Kovalchuk <a.kovalchuk@gmarkg.com>
2024-02-11 20:09:43 +0200

    created composes in styles for FriendListItem component

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,13 +2,8 @@ import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className={styles.item}>
-      {isOnline ? (
-        <span className={styles.onLine}></span>
-      ) : (
-        <span className={styles.offLine}></span>
-      )}
-      
+    <li className={styles.item}>    
+      <span className={isOnline ? styles.onLine : styles.offLine}></span>
       <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={styles.name}>{name}</p>
     </li>
